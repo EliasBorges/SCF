5d3955c64fe636d10fd83456c7b73a70b5621f35
Bruno Müller Araujo Godinho <bwk.bruno@gmail.com>
2017-10-27 15:22:41 -0200

    -layout

---
+++ SCF/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCF.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ViewResult Contact()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
