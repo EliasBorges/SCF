1102b993da6aa7a25077d610cca4fd5657ecfd6b
Elias Junio <elias.ti@outlook.com.br>
2017-10-27 23:04:09 -0200

    - Reajuste view login

---
+++ SCF/Controllers/ManterLoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCF.Controllers
+{
+    public class ManterLoginController : Controller
+    {
+        // GET: ManterLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ManterLogin()
+        {
+            return View();
+        }
+    }
+}
